32d65f1a5fb54067ace2174b6f3cabad8ee43a24
LinvalM <linvle2@gmail.com>
2019-04-15 22:44:59 +0200

    added nodemon

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
+//site to learn this tutorial https://getbuzz.io/c/learning-expressjs
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');//represents the route exported by index.js from the routes directory
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));//we include the views in a directory called views
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -19,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter);//when the route '/' is typed we are making a call to the indexRouter to handle that for us
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -39,3 +40,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+//http protocol exposes many methods to us
+//the first method is http.get-> allows us to fetch any resources from the web server
+//in our index router example server will respond with res.render() a special function that comes from expres that renders html files
+//server sed=nd a web page
+//http.post-> which is used to submit some data to the web server//server updates some data and res.redirect will redirect user to a new route
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+let index=require('../controllers/index');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//for the http-get method this is what we will do the way we will handle such request on the route
 
+router.get('/',index.index );//we passing the index handler from controllers  to handle this route
+//defined a route that will handle the '/' route by calling a callback function that will do the stuff in its body
+//it will respond to the matched route only
 module.exports = router;
+//we pass out the module for other files to use it

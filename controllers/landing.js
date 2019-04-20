61e89d5346fd5d1e53b9e14b6a20fbb01f834271
LinvalM <linvle2@gmail.com>
2019-04-20 16:24:43 +0200

    showing leads from the database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,24 @@
 //controllers allow us to seperate handler logic from
 //route handler logic
 //exporting this method as our handler
+const models=require('../models');
 exports.get_landing=function(req, res, next) {
     res.render('landing', { title: 'Express' });
 };
 exports.submit_lead=function(req, res, next) {
   console.log("lead email:",req.body.lead_email);
-  res.redirect('/');
+  return models.Lead.create({//create is a sequelize method 
+      email:req.body.lead_email
+  }).then(lead=>{
+      res.redirect('/leads');
+  });
+
+};
+//using this handler to list the leads in the database
+exports.show_leads=function(req, res, next) {
+	//findAll() method is defined by sequelize 
+    models.Lead.findAll().then(
+    	leads=>{//leads contains the rows returned from the database and the methods to access those rows
+    		res.render('landing',{title:'Express',leads:leads});//use the same landing viewfile to load the pages
+    	})
 };
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ let landing=require('../controllers/landing');
 /* GET home page. */
 //for the http-get method this is what we will do the way we will handle such request on the route
 
+
+//Creation routes
 router.get('/',landing.get_landing );
 router.post('/',landing.submit_lead);
 //we passing the index handler from controllers  to handle this route
 //defined a route that will handle the '/' route by calling a callback function that will do the stuff in its body
 //it will respond to the matched route only
+
+//listing the leads
+router.get('/leads',landing.show_leads);
 module.exports = router;
 //we pass out the module for other files to use it
